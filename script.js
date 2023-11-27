84b74e1a4214733a09909f00ce940920e6f2016b
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-27 20:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //your JS code here. If required.
-let input1 = prompt(enter first number);
-let input2 = prompt(enter second numbr);
- let calc =function(num1 , num2) {
+let input1 = prompt("Enter the first number");
+let input2 = prompt("Enter the second number");
+ let calc = function(num1 , num2) {
 	
-	alret((num1*2)+num2);
-	 
-}
-calc(input1 , input2);
+	alert("The final result is:" + ((num1 * 2) + (num2)));
+};
+calc(input1 ,Number(input2));
+
+
 
 
 
